26cc2f19976549fa69d4787e0d404c0444767cf8
Fabry Exan (s148939) <exan.fabry@student.ap.be>
2026-02-26 16:37:35 +0100

    Add style

--- src/app/pages/blog/blog.ts
+++ src/app/pages/blog/blog.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
+import { blogPosts } from '../../blog-posts-array';
 
 @Component({
   selector: 'app-blog',
@@ -8,7 +9,13 @@ import { RouterLink } from '@angular/router';
   templateUrl: './blog.html',
   styleUrl: './blog.css',
 })
-export class Blog {
+export class Blog implements OnInit {
+  public posts: string[][][] = [[]];
+
   http = inject(HttpClient);
   constructor() { }
+
+  ngOnInit() {
+    this.posts = blogPosts;
+  }
 }
