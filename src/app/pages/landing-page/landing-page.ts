2423a1f9a204106fdb097cd97801a85f3c539a1c
Fabry Exan (s148939) <exan.fabry@student.ap.be>
2026-03-22 23:39:06 +0100

    Add content to landing page.

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -1,13 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
+import { blogPosts } from '../../blog-posts-array';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
 export class LandingPage {
+  public posts: string[][][] = [[]];
+
   http = inject(HttpClient);
   constructor() { }
+
+  ngOnInit() {
+    this.posts = blogPosts;
+  }
 }
